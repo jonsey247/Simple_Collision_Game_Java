af35aad77516b7409ed7873740510eb1d88b7ddf
jonsey247 <jacobfilmjones@hotmail.com>
2017-09-04 20:15:42 +0100

    collision detection added. health goes down

--- src/com/TestGame/main/Game.java
+++ src/com/TestGame/main/Game.java
@@ -28,8 +28,8 @@ public class Game extends Canvas implements Runnable {
 
         r = new Random();
 
-        handler.addObject(new Player(WIDTH / 2 - 32, HEIGHT / 2 - 32, ID.Player));
-        for(int i = 0; i < 10; i++)
+        handler.addObject(new Player(WIDTH / 2 - 32, HEIGHT / 2 - 32, ID.Player, handler));
+
         handler.addObject(new BasicEnemy(r.nextInt(WIDTH), r.nextInt(HEIGHT), ID.BasicEnemy));
     }
 
--- src/com/TestGame/main/Player.java
+++ src/com/TestGame/main/Player.java
@@ -6,10 +6,16 @@ import java.util.Random;
 public class Player extends GameObject {
 
     Random r = new Random();
+    Handler handler;
 
-    public Player(int x, int y, ID id) {
+    public Player(int x, int y, ID id, Handler handler) {
         super(x, y, id);
+        this.handler = handler;
 
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, 32, 32);
     }
 
     public void tick() {
@@ -19,9 +25,25 @@ public class Player extends GameObject {
         x = Game.clamp(x, 0, Game.WIDTH - 32);
         y = Game.clamp(y, 0, Game.HEIGHT - 32);
 
+        collision();
+
+    }
+
+    public void collision() {
+        for(int i = 0; i < handler.object.size(); i++){
+            GameObject tempObject = handler.object.get(i);
+
+            if(tempObject.getId() == ID.BasicEnemy) {
+                if(getBounds().intersects(tempObject.getBounds())) {
+                    //collision code
+                    HUD.HEALTH -= 2;
+                }
+            }
+        }
     }
 
     public void render(Graphics g) {
+
         g.setColor(Color.white);
         g.fillRect(x, y, 32, 32);
     }
