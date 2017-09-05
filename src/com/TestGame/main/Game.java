ee73047643a2ed81ee869e8bd086be7660e01e32
jonsey247 <jacobfilmjones@hotmail.com>
2017-09-05 14:41:58 +0100

    score and level implemented in HUD

--- src/com/TestGame/main/Game.java
+++ src/com/TestGame/main/Game.java
@@ -15,6 +15,7 @@ public class Game extends Canvas implements Runnable {
     private Random r;
     private Handler handler;
     private HUD hud;
+    private Spawn spawner;
 
 
     public Game() {
@@ -25,12 +26,11 @@ public class Game extends Canvas implements Runnable {
         new Window(WIDTH, HEIGHT, "Let's Build A Game!!", this);
 
         hud = new HUD();
+        spawner = new Spawn(handler, hud);
 
         r = new Random();
 
         handler.addObject(new Player(WIDTH / 2 - 32, HEIGHT / 2 - 32, ID.Player, handler));
-        for(int i = 0; i < 5; i++)
-        handler.addObject(new BasicEnemy(r.nextInt(WIDTH), r.nextInt(HEIGHT), ID.BasicEnemy, handler));
     }
 
     public synchronized void start() {
--- src/com/TestGame/main/HUD.java
+++ src/com/TestGame/main/HUD.java
@@ -6,16 +6,30 @@ public class HUD {
 
     public static int HEALTH = 100;
 
+    private int greedValue = 255;
+
+    private int score = 0;
+    private int level = 1;
+
     public void tick() {
+
         HEALTH = Game.clamp(HEALTH,0, 100);
+
+        greedValue = Game.clamp(greedValue, 0, 255);
+
+        greedValue = HEALTH * 2;
+
     }
 
     public void render(Graphics g) {
         g.setColor(Color.gray);
         g.fillRect(15, 15, 200, 32);
-        g.setColor(Color.green);
+        g.setColor(new Color(75, greedValue, 0));
         g.fillRect(15, 15, HEALTH * 2, 32);
         g.setColor(Color.white);
         g.drawRect(15, 15, 200, 32);
+
+        g.drawString("Score: " + score, 9, 64);
+        g.drawString("Level: " + level, 10, 80);
     }
 }
